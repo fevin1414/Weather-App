810629c44ccdd23a66e5d0ef106c14a11c59cf43
fevin1414 <fevinbiju1414@gmail.com>
2024-04-03 23:28:13 -0500

    removed console log

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -24,7 +24,7 @@ const useWeather = ({ location }) => {
         }
         const result = await response.json();
         setData(result);
-        console.log(data);
+
         setLoading(false);
       } catch (err) {
         setError(err);
